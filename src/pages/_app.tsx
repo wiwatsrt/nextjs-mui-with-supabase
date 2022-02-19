a5aeddbadac389148ae8052a1f1cc24b01c67502
Wiwat Srisattha <wiwat_srt@hotmail.com>
2022-02-19 09:56:58 +0700

    feat: add Mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import { AppProps } from 'next/app';
-import '@/styles/global.css';
+import { AppProps } from 'next/app'
+import theme from '@/styles/theme'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import createEmotionCache from '@/utils/createEmotionCache'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
